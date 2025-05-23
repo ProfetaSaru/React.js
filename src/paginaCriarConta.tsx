98a99e62342bd88a4b6d992a25878f3e22e32618
Fernando Caldeira <maneiro448@gmail.com>
2025-05-23 17:04:09 -0300

    modified:   src/paginaCriarConta.tsx

--- src/paginaCriarConta.tsx
+++ src/paginaCriarConta.tsx
@@ -3,11 +3,11 @@ import CreateAccount from "./components/CriadorConta.tsx";
 import Footer from "./components/Footer.tsx";
 
 function PaginaCriarConta() {
-  const Logo = "./icons/3135715.png";
+  const logo = "./icons/3135715.png";
 
   return  (
     <>
-    {NavigationBar(Logo, false)}
+    <NavigationBar logoDaEmpresa={logo} logado={false} />
     {CreateAccount()}
     {Footer()}
     </>
